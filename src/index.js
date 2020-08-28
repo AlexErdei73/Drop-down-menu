3f34d3c8b12b6670c9a1af34114442b50d40bdcf
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-08-28 01:40:36 +0100

    further bugfix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-
-export function DropDownMenu(container, onItemClick) {
-    this.container = container;
-    this.onItemClick = onItemClick;
-    const list = container.querySelector('ul');
-    const button = container.querySelector('div');
-
-    list.classList.add('hide');
-
-    button.addEventListener('click', () => {
-        if (list.classList.contains('hide')) {
-            list.classList.remove('hide');
-            list.classList.add('show');
-        } else {
-            list.classList.remove('show');
-            list.classList.add('hide');
-        }
-    })
-
-    const items = list.querySelectorAll('li');
-    items.forEach((li) => {
-        li.addEventListener('click', this.onItemClick);
-    })
-        
-}
-
+import DropDownMenu from '@alexerdei73/drop-down-menu';
 
 const menu = new DropDownMenu(document.querySelector('#dropdownmenu'), onMenuItemClick);
 
@@ -31,6 +6,3 @@ function onMenuItemClick(e) {
     const result = document.querySelector('#result');
     result.textContent = e.target.textContent;
 }
-
-
-
